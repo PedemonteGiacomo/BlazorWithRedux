8776b7c25e170b6831a9242b05971ddae49a45c8
Giacomo Pedemonte <giacomo.pedemonte@esaote.com>
2024-03-14 12:13:02 +0100

    Add test

---
+++ CounterTestsFixture.cs
@@ -0,0 +1,18 @@
+﻿using BlazorWithRedux.Store.Counter.State;
+using Fluxor;
+
+namespace UnitTestsForBlazorWithRedux
+{
+    public class CounterTestsFixture
+    {
+        public IDispatcher dispatcher { get; set; }
+        public UndoableCounterState state { get; set; }
+
+        public CounterTestsFixture()
+        {
+            // Create or configure mock instances here
+            dispatcher = new Dispatcher();
+            state = new() { Present = new CounterState { Count = 0 } };
+        }
+    }
+}
