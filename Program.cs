e3f39e0871cdd44186da70d4ed6f7a1bcfbb863b
Giacomo Pedemonte <giacomo.pedemonte@esaote.com>
2024-03-28 11:06:00 +0100

    add logging middleware

--- Program.cs
+++ Program.cs
@@ -2,6 +2,13 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorWithRedux;
 using Fluxor;
+using OpenTelemetry;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using OpenTelemetry.Exporter;
+using System.Diagnostics;
+using OpenTelemetry.Instrumentation;
+using BlazorWithRedux.Store.Middleware.Logging;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,11 +16,36 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+var serviceName = "LoggingMiddleware";
+var serviceVersion = "1.0.0";
+var otlpEndpoint = "http://172.212.16.128";
+
+// Add OpenTelemetry
+builder.Services.AddOpenTelemetry()
+    .WithTracing(builder =>
+    {
+        builder
+            .AddSource(serviceName)
+            .SetSampler(new AlwaysOnSampler())
+            .ConfigureResource(resource =>
+                  resource.AddService(
+                    serviceName: serviceName,
+                    serviceVersion: serviceVersion
+                  )
+             )
+            .AddHttpClientInstrumentation()
+            .AddOtlpExporter(opt =>
+            {
+                opt.Endpoint = new Uri(otlpEndpoint);
+                opt.Protocol = OtlpExportProtocol.Grpc;
+            });
+    });
+
 builder.Services.AddFluxor(options =>
-{
     options
       .ScanAssemblies(typeof(Program).Assembly)
-      .UseReduxDevTools(); // optional, in production you can remove this line because is for debugging purposes
-});
+      .AddMiddleware<LoggingMiddleware>()
+      .UseReduxDevTools()// optional, in production you can remove this line because is for debugging purposes
+); 
 
 await builder.Build().RunAsync();
---
+++ Store/Middleware/Logging/LoggingMiddleware.cs
@@ -0,0 +1,72 @@
+﻿using Fluxor;
+using OpenTelemetry;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using System.Diagnostics;
+
+namespace BlazorWithRedux.Store.Middleware.Logging
+{
+    public class LoggingMiddleware : IMiddleware
+    {
+        private IStore Store;
+        private IDispatcher Dispatcher;
+
+        private static readonly ActivitySource MyActivitySource = new ActivitySource("LoggingMiddleware");
+
+        public void AfterDispatch(object action)
+        {
+            //tracer.StartActiveSpan($"Action {action.GetType().Name} dispatched", SpanKind.Client); // , SpanKind.Client
+            using (var activity = MyActivitySource.StartActivity("AfterDispatch"))
+            {
+                Console.WriteLine($"Action {action.GetType().Name} dispatched");
+            }
+        }
+
+        public void AfterInitializeAllMiddlewares()
+        {
+            using (var activity = MyActivitySource.StartActivity("AfterInitializeAllMiddlewares"))
+            {
+                Console.WriteLine(nameof(AfterInitializeAllMiddlewares));
+            }
+            //tracer.StartActiveSpan(nameof(AfterInitializeAllMiddlewares), SpanKind.Client); // , SpanKind.Client
+        }
+
+        public void BeforeDispatch(object action)
+        {
+            using (var activity = MyActivitySource.StartActivity("BeforeDispatch"))
+            {
+                Console.WriteLine($"Action {action.GetType().Name} is about to be dispatched");
+            }
+            //tracer.StartActiveSpan($"Action {action.GetType().Name} is about to be dispatched", SpanKind.Client);
+        }
+
+        public IDisposable BeginInternalMiddlewareChange()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task InitializeAsync(IDispatcher dispatcher, IStore store)
+        {
+            Store = store;
+            Dispatcher = dispatcher;
+            using (var activity = MyActivitySource.StartActivity("InitializeAsync"))
+            {
+                Console.WriteLine(nameof(InitializeAsync));
+            }
+            //tracer.StartActiveSpan(nameof(InitializeAsync), SpanKind.Client);
+
+            return Task.CompletedTask;
+        }
+
+        public bool MayDispatchAction(object action)
+        {
+            //tracer.StartActiveSpan(nameof(MayDispatchAction) + action.GetType().Name, SpanKind.Client);
+            using (var activity = MyActivitySource.StartActivity("MayDispatchAction"))
+            {
+                Console.WriteLine(nameof(MayDispatchAction) + action.GetType().Name);
+            }
+            return true;
+        }
+    }
+}
